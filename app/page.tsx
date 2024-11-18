45908a6281c6c274f5885f4abc6628a69ff0eb90
Avery Bai <jerryqueue@gmail.com>
2024-11-18 18:22:46 +0800

    Added delete functionality and authenticator

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
+import {useState, useEffect} from "react";
+import {useAuthenticator} from "@aws-amplify/ui-react";
+import {generateClient} from "aws-amplify/data";
+import type {Schema} from "@/amplify/data/resource";
 import "./../app/app.css";
-import { Amplify } from "aws-amplify";
+import {Amplify} from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
@@ -14,6 +15,8 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  const {signOut} = useAuthenticator();
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -31,22 +34,32 @@ export default function App() {
     });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({id})
+  }
+
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+      <main>
+        <h1>My todos</h1>
+        <button onClick={createTodo}>+ new</button>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}
+                onClick={() => deleteTodo(todo.id)}
+            >
+              {todo.content}
+            </li>
+          ))}
+        </ul>
+        <div>
+          🥳 App successfully hosted. Try creating a new todo.
+          <br/>
+          <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+            Review next steps of this tutorial.
+          </a>
+        </div>
+
+        <button onClick={signOut}>Sign out</button>
+      </main>
   );
 }
